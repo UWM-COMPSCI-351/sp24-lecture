534fc9130481577752603db888fc566c8cbce80d
boyland <boyland@UWM014851.ad.uwm.edu>
2024-02-08 10:52:58 -0600

    Added constructor and simple ds

--- src/edu/uwm/cs351/RangeCollection.java
+++ src/edu/uwm/cs351/RangeCollection.java
@@ -1,5 +1,30 @@
 package edu.uwm.cs351;
 
-public class RangeCollection {
+import java.util.AbstractCollection;
+import java.util.Iterator;
+
+public class RangeCollection extends AbstractCollection<Integer> {
+	private final int lo, hi;
+	
+	/**
+	 * Create a range from lo to hi
+	 * @param lo inclusive lower bound
+	 * @param hi exclusive upper bound
+	 */
+	public RangeCollection(int lo, int hi) {
+		this.lo = lo;
+		this.hi = hi;
+	}
+
+	@Override // required
+	public Iterator<Integer> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override // required
+	public int size() {
+		return hi - lo;
+	}
 
 }

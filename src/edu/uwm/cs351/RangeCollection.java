3d2d44d8370b29b710941f7e5de3e48505b6c95d
boyland <boyland@UWM014851.ad.uwm.edu>
2024-02-08 11:18:19 -0600

    after lecture

--- src/edu/uwm/cs351/RangeCollection.java
+++ src/edu/uwm/cs351/RangeCollection.java
@@ -18,8 +18,7 @@ public class RangeCollection extends AbstractCollection<Integer> {
 
 	@Override // required
 	public Iterator<Integer> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new MyIterator();
 	}
 
 	@Override // required
@@ -27,4 +26,26 @@ public class RangeCollection extends AbstractCollection<Integer> {
 		return hi - lo;
 	}
 
+	@Override // efficiency
+	public boolean contains(Object o) {
+		if (!(o instanceof Integer)) return false;
+		Integer i = (Integer)o;
+		return lo <= i && i < hi;
+	}
+
+	private class MyIterator implements Iterator<Integer> {
+		int current = lo-1;
+		
+		@Override // required
+		public boolean hasNext() {
+			return current+1 < hi;
+		}
+
+		@Override // required
+		public Integer next() {
+			++current;
+			return current;
+		}
+		
+	}
 }

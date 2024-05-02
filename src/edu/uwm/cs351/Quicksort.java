10f579b9e9fbe6012dd21da0fc29c1eb034336b4
John Boyland <boyland@uwm.edu>
2024-05-02 09:38:53 -0500

    Added Quicksort stub

---
+++ src/edu/uwm/cs351/Quicksort.java
@@ -0,0 +1,16 @@
+package edu.uwm.cs351;
+
+import java.util.Comparator;
+
+public class Quicksort<T> {
+
+	private final Comparator<T> comparator;
+	
+	public Quicksort(Comparator<T> c) {
+		comparator = c;
+	}
+	
+	public void sort(T[] array) {
+		
+	}
+}
